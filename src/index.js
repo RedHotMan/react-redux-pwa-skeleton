2c04ea05bf286079d2994d1346049b14a3b0777c
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-12-04 18:51:01 +0100

    use browser router
    fix

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./css/index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import createHashHistory from "history/createHashHistory";
-
-const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </Router>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
