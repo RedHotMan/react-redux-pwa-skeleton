01330b4df7e2c3ff65b223597fafb2676cbd8e1c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-29 14:25:43 +0000

    Deploy RedHotMan/react-redux-pwa-skeleton to github.com/RedHotMan/react-redux-pwa-skeleton.git:gh-pages

---
+++ precache-manifest.9225c03da152ca23d8c0cbec9bfe85dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8a47f33a265775b97e5",
+    "url": "/react-redux-pwa-skeleton/static/css/main.9b03af0c.chunk.css"
+  },
+  {
+    "revision": "a8a47f33a265775b97e5",
+    "url": "/react-redux-pwa-skeleton/static/js/main.a8a47f33.chunk.js"
+  },
+  {
+    "revision": "d2786638908fe76cbb59",
+    "url": "/react-redux-pwa-skeleton/static/js/1.d2786638.chunk.js"
+  },
+  {
+    "revision": "9902f1ab8db3a2659269",
+    "url": "/react-redux-pwa-skeleton/static/js/runtime~main.9902f1ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-redux-pwa-skeleton/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7d773b32300fbe69c27e22e0309b3f8b",
+    "url": "/react-redux-pwa-skeleton/index.html"
+  }
+];
